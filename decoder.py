c734025a52b3a4de2a23ac54ef70fbe40ef3b913
rbih-boulanouar <boulanouar.rbih@univ-constantine2.dz>
2022-04-30 15:15:48 +0100

    add decode type checker

--- decoder.py
+++ decoder.py
@@ -4,19 +4,44 @@ import html
 
 class decode:
 
+    #   check if string is base64 encoded
+
+    def isBase64(string):
+        try:
+            return base64.b64encode(base64.b64decode(string)) == string
+        except Exception:
+            return False
+
     #   decode Base64
 
     def Base64(coded_string):
         return base64.b64decode(coded_string)
 
-    #   decode url encoding
+    #   check if string is urlencoded
+
+    def isUrlEncoded(string):
+        try:
+            return urllib.parse.quote(urllib.parse.unquote(string)) == string
+        except Exception:
+            return False
+
+    #   decode urlencode
 
     def url(coded_string):
         return urllib.parse.unquote(coded_string)
 
+    #   check if string is HTMLentitie
+
+    def isHTMLEntitie(string):
+        try:
+            return html.escape(html.unescape(string)) == string
+        except Exception:
+            return False
+
+
     #   Decode HTML entities
 
-    def  html_entities(coded_string):
+    def  html_entitie(coded_string):
         return html.unescape(coded_string)
 
     #   we can add any type of encoding we want based on the Web application 
